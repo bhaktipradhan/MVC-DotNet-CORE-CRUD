989dcd7c17575d776fe3d455759169cb54c29ecd
bhaktipradhan <bhaktipradhan07@gmail.com>
2024-06-18 09:31:53 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using studentsMVC.Models.Entities;
+using System.Data;
+
+namespace studentsMVC.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions <ApplicationDbContext> options): base(options) 
+        {
+            
+        }
+
+        public DbSet<Student>Students { get; set; }
+    }
+}
